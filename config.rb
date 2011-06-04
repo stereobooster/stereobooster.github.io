36d6688c543f8037af3237a50fe77bd0b63c00d5
slavic <stereobooster@gmail.com>
2011-06-04 22:19:21 +0300

    method67.com

--- config.rb
+++ config.rb
@@ -9,6 +9,7 @@ sass_dir = "_sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
